c88f9a05348d8df1a03754c144da6d6678d0c8cb
liuxiong <liuxiong332@163.com>
2020-03-28 12:29:59 +0800

    Add http service to process video

--- device_service/device_manager.py
+++ device_service/device_manager.py
@@ -2,40 +2,43 @@
 from model import object_detection
 from multiprocessing import Process, Pipe
 
-def device_process(conn): 
-  object_detection.ModelRunner().run()
+
+def device_process(conn):
+    object_detection.ModelRunner(conn).run()
+
 
 class DeviceManager:
-  def __init__(self):
-    self.device_ids = []
-    self.tasks = {}
-    self.conns = {}
+    def __init__(self):
+        self.device_ids = []
+        self.tasks = {}
+        self.conns = {}
 
-  def add_device(self, device_id):
-    if not hasattr(self, "device_ids"):
-      self.device_ids = []
-    if device_id not in self.device_ids:
-      self.device_ids.append(device_id)
-      self.start_task(device_id)
+    def add_device(self, device_id):
+        print("Start new device", device_id)
+        if not hasattr(self, "device_ids"):
+            self.device_ids = []
+        if device_id not in self.device_ids:
+            self.device_ids.append(device_id)
+            self.start_task(device_id)
 
-  def del_device(self, device_id):
-    if device_id in self.device_ids:
-      self.conns[device_id].send("quit")
-      self.tasks[device_id].join()
-      self.conns[device_id].close()
-      self.device_ids.remove(device_id)
-      del self.conns[device_id]
-      del self.tasks[device_id]
+    def del_device(self, device_id):
+        print("Will delete device", device_id)
+        if device_id in self.device_ids:
+            self.conns[device_id].send("quit")
+            self.tasks[device_id].join()
+            self.conns[device_id].close()
+            self.device_ids.remove(device_id)
+            del self.conns[device_id]
+            del self.tasks[device_id]
 
-  def start_task(self, device_id):
-    parent_conn, child_conn = Pipe()
-    p = Process(target=device_process, args=(child_conn, ))
-    p.start()
-    self.tasks[device_id] = p
-    self.conns[device_id] = parent_conn
-    print("start task", device_id, self.tasks, self.conns)
+    def start_task(self, device_id):
+        parent_conn, child_conn = Pipe()
+        p = Process(target=device_process, args=(child_conn, ))
+        p.start()
+        self.tasks[device_id] = p
+        self.conns[device_id] = parent_conn
+        print("start task", device_id, self.tasks, self.conns)
 
-  def join(self):
-    for d in self.device_ids:
-      self.del_device(d)
-    
+    def join(self):
+        for d in self.device_ids:
+            self.del_device(d)
--- main.py
+++ main.py
@@ -3,9 +3,37 @@
 # object_detection.ModelRunner().run()
 
 from device_service import device_manager
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import re
+
+device_manager = device_manager.DeviceManager()
+
+
+class ServerHTTP(BaseHTTPRequestHandler):
+
+    def do_POST(self):
+        print("Get path", self.path)
+        search_res = re.search(r"/device/add\?deviceId=(\d+)", self.path)
+        if search_res is not None:
+            device_manager.add_device(search_res.group(1))
+            self.send_response(200, "ok")
+            return self.wfile.write(b"ok")
+
+        search_res = re.search(r"/device/del\?deviceId=(\d+)", self.path)
+        if search_res is not None:
+            device_manager.del_device(search_res.group(1))
+            self.send_response(200, "ok")
+            return self.wfile.write(b"ok")
+
+        return self.send_response(500, "unknown command")
+
 
 if __name__ == '__main__':
-  dm = device_manager.DeviceManager()
-  dm.add_device("1")
+    # dm = device_manager.DeviceManager()
+    # dm.add_device("1")
 
-  dm.join()
+    # dm.join()
+    http_server = HTTPServer(('', 1999), ServerHTTP)
+    print("Server start listen on ", 1999)
+    http_server.serve_forever()
+    device_manager.join()
